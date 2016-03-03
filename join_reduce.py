223d0316d501af6ba0748f34c854e185b58c8ffd
Youssef Ahres <youssefahres@github.com>
2016-03-03 11:36:17 -0800

    debug

--- join_reduce.py
+++ join_reduce.py
@@ -7,7 +7,27 @@ from itertools import groupby
 from operator import itemgetter
 import time
 import sys
-from misc import in_square, haversine
+from math import radians, cos, sin, asin, sqrt
+import json
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 3956  # Radius of earth in miles. Use 6371 for kms
+    return c * r
+
+def in_square(lon, lat):
+    return (lon < -73.3502) and (lon > -74.5532) and (lat > 40.2858) and (lat < 41.1125)
 
 MAX_DISTANCE = 100 # 100 miles
 MAX_TIME = 86400 # one day in seconds
